d960d2ea5ec495ebd527e1b53b6eb1487bb710af
“Mudabbir” <sohailmudabbir@gmail.com>
2025-01-02 18:30:57 +0530

    SHA1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,12 +17,12 @@ const Hero = () => {
       
 
         {/* Why DLVB IMPEX Section */}
-        <div className="max-w-7xl mx-auto px-6 py-16">
-          <h2 className="text-4xl font-bold text-center text-gray-800 mb-16">
+        <div className="max-w-7xl mx-auto  px-6 py-16">
+          <h2 className="text-4xl mt-28 font-bold text-center text-gray-800 mb-16">
             Why DLVB IMPEX?
           </h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
             <div className="p-8 bg-green-100 rounded-3xl transition-transform hover:scale-105">
               <img src={icon2} alt="Vision" className="w-28 z-10 mb-6" />
               <h3 className="text-2xl font-bold mb-4">Vision</h3>
